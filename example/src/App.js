dbb0237d4ffa16aa6b9668a488e5a03b183d64fa
Michal Sevcik <sevcik.mi@gmail.com>
2018-09-22 22:20:19 +0200

    - update App.js example

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
-import Countdown from 'react-downcount'
+import Countdown, { doubleDigit } from 'react-downcount'
+
+// eslint-disable-next-line
+const customRenderer = ({ days, hrs, mins, secs }) => {
+  return <Fragment>{days > 0 && `${days} days `} {`${hrs} h`} {`${doubleDigit(mins)} m`} {`${doubleDigit(secs)} s`}</Fragment>
+}
 
 export default class App extends Component {
   render () {
     const endDate = new Date()
-    endDate.setMinutes(endDate.getMinutes() + 3)
+    endDate.setDate(endDate.getDate() + 1)
+    endDate.setSeconds(endDate.getSeconds() + 20)
 
     return <Countdown
       endDate={endDate}
       onCompleted={() => {
         console.log('The countdown has completed')
-      }} />
+      }}>
+      {customRenderer}
+    </Countdown>
   }
 }
